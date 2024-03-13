4868902f7e0e213b7cf413af40e53dcb450ca8de
Natwar Patidar <natvarp6062@gmail.com>
2024-03-14 00:12:41 +0530

    add code

---
+++ Searching/guessNumber.js
@@ -0,0 +1,26 @@
+var guess = function (mid) {
+    let number = 10;
+    if (mid > number) {
+        return -1;
+    } else if (mid < number) {
+        return 1;
+    } else if (mid == number) {
+        return 0;
+    }
+}
+
+var guessNumber = function (n) {
+    let lo = 1;
+    let hi = n;
+    while (lo <= hi) {
+        let mid = lo + Math.floor((hi - lo) / 2);
+        if (guess(mid) == -1) {
+            hi = mid - 1;
+        } else if (guess(mid) == 1) {
+            lo = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
