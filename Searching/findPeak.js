8f191f32cc4edf2fba34b81622909c67a491aae1
Natwar Patidar <natvarp6062@gmail.com>
2024-03-17 23:30:28 +0530

    add code

--- Searching/findPeak.js
+++ Searching/findPeak.js
@@ -4,7 +4,7 @@ var peak = function (nums) {
     let lo = 0, hi = n - 1;
     while (lo <= hi) {
         let mid = lo + Math.flooe((hi - lo) / 2);
-        if () {}
+        if (nums[mid] < nums[mid+1] && nums[mid] > nums[mid-1]) return mid;
         if (nums[mid] < n - 1 && nums[mid] < nums[mid+1]) {
             lo = mid + 1;
         } else {
