35b4999b02f754d527a40ffe7c13ad847479407b
Natwar Patidar <natvarp6062@gmail.com>
2024-03-18 00:25:05 +0530

    add code

--- Searching/findPeak.js
+++ Searching/findPeak.js
@@ -4,7 +4,8 @@ var findPeakElement = function (nums) {
     if(n == 1) return 0;
     while (lo < hi) {
         let mid = lo + Math.floor((hi - lo) / 2);
-        if (mid === 0) return nums[0] >= nums[1] ? 0 : 1;
+        if (mid === 0 && nums[mid] > nums[mid+1]) return mid;
+        if (mid === hi && nums[mid] > nums[mid-1]) return mid;
         if (nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]) return mid;
 
         if(nums[mid] < nums[mid+1]) {
