328da8c66fe7b2b32983a62f44ad15c736b41545
Natwar Patidar <natvarp6062@gmail.com>
2024-03-19 22:25:34 +0530

    add code

--- Searching/spellsAndPotions.js
+++ Searching/spellsAndPotions.js
@@ -24,3 +24,6 @@ var successfulPairs = function(spells, potions, success) {
 
     return pairs;
 };
+
+let spells = [3,1,2], potions = [8,5,8], success = 16;
+console.log(successfulPairs(spells, potions, success));
