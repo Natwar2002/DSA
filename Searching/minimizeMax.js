7a0f81db389e8e2256d1badd39d9f033b73ce276
Natwar Patidar <natvarp6062@gmail.com>
2024-03-23 00:24:20 +0530

    add code

--- Searching/minimizeMax.js
+++ Searching/minimizeMax.js
@@ -5,7 +5,7 @@ var minimizeMax = function(nums, p) {
 
     while (lo <= hi) {
         let mid = lo + Math.floor((hi - lo) / 2);
-        if (canFormPpairsWithLessThanMid(nums, mid, p)) {
+        if (canFormPpairsWithMinimumMaxDifference(nums, mid, p)) {
             ans = mid;
             hi = mid - 1;
         } else lo = mid + 1;
@@ -13,7 +13,7 @@ var minimizeMax = function(nums, p) {
     return ans
 }
 
-function canFormPpairsWithLessThanMid(nums, mid, p) {
+function canFormPpairsWithMinimumMaxDifference(nums, mid, p) {
     let count = 0, i = 0;
     while (i < nums.length - 1) {
         if (nums[i+1] - nums [i] <= mid) {
