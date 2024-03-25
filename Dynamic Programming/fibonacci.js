a7a51574ff283a56eb927bc34444169fadeb62c6
Natwar Patidar <natvarp6062@gmail.com>
2024-03-25 22:42:11 +0530

    add code

--- Dynamic Programming/fibonacci.js
+++ Dynamic Programming/fibonacci.js
@@ -4,16 +4,26 @@
 } */
 
 let dp;
+
+function f_bu (n) {
+    dp = Array(n+1);
+    dp[0] = 0;
+    dp[1] = 1;
+    for(let i = 2; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+} 
+
 function f (n) {
     if(n === 0 || n === 1) return n;
-
     if (dp[n] != -1) return dp[n];
     return dp[n] = f(n-1) + f(n-2);
 }
 
 var fib = function (n) {
-    dp = Array(n+1).fill(-1);
-    return f(n);
+    // dp = Array(n+1).fill(-1);
+    return f_bu(n);
 }
 
 let n = 10;
