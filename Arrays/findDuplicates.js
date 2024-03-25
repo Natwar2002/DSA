acd63c8552462dffe5832411bedc506d8ba08a7f
Natwar Patidar <natvarp6062@gmail.com>
2024-03-25 23:56:19 +0530

    add code

---
+++ Arrays/findDuplicates.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function(nums) {
+    const ans = [];
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        const x = Math.abs(nums[i]);
+        if (nums[x - 1] < 0) {
+            ans.push(x);
+        }
+        nums[x - 1] *= -1;
+        console.log(x);
+    }
+    return ans;
+};
+
+let arr = [4,3,2,7,8,2,3,1];
+console.log(findDuplicates(arr));
