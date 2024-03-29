79cc6140da9abb8c8fa791acb2d7038b8abf2154
Natwar Patidar <natvarp6062@gmail.com>
2024-03-29 23:43:32 +0530

    add code

---
+++ Dynamic Programming/coinChange.js
@@ -0,0 +1,15 @@
+let c;
+let dp;
+
+var f = function (amount) {
+    if (amount < 0) return Number.MAX_SAFE_INTEGER;
+    if (amount === 0) return 0;
+    if (dp[amount] != -1) return dp[amount];
+
+    let ans = Number.MAX_SAFE_INTEGER;
+    for()
+}
+
+var coinChange = function (amount, coins) {
+    
+}
