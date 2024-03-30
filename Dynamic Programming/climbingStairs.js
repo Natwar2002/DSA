b97c9247406c58565eb359c3409e851f1da0e07e
Natwar Patidar <natvarp6062@gmail.com>
2024-03-30 23:23:55 +0530

    add code

--- Dynamic Programming/climbingStairs.js
+++ Dynamic Programming/climbingStairs.js
@@ -1,10 +1,14 @@
 let dp;
 var f = function (n) {
-    if(n == 0 || n == 1 || n == 2) return n;
-    
+    if(n == 0 || n == 1 || n == 2 || n == 3) return n;
+    if(dp[n] !== -1) return dp[n];
+    return dp[n] = f(n-1) + f(n-2);
 };
 
 var climbingStairs = function (n) {
     dp = Array(n+1).fill(-1);
     return f(n);
 };
+
+let n = 4;
+console.log(climbingStairs(n));
