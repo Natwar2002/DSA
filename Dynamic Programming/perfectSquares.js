4487880bc17d395fbd6f8345b3574ba694db698a
Natwar Patidar <natvarp6062@gmail.com>
2024-03-30 22:47:01 +0530

    add code

---
+++ Dynamic Programming/perfectSquares.js
@@ -0,0 +1,34 @@
+let c;
+let dp;
+
+var f = function (n) {
+    // if(n < 0) return Number.MAX_SAFE_INTEGER;
+    if(n === 0) return 0;
+    if(dp[n] != -1) return dp[n];
+
+    let ans = Number.MAX_SAFE_INTEGER;
+    for(let i = 0; i < c.length; i++) {
+        if(n >= c[i]) {
+            ans = Math.min(ans, f(n-c[i]));
+        } else {
+            break;
+        }
+    }
+
+    if(ans === Number.MAX_SAFE_INTEGER) return dp[n] = Number.MAX_SAFE_INTEGER;
+    return dp[n] = 1 + ans;
+};
+
+var numSquares = function(n) {
+    c = Array(n+1);
+    let j = 0;
+    for (let i = 1; i*i <= n; i++) {
+        c[j] = i*i;
+        j++;
+    }
+    dp = Array(n+1).fill(-1);
+    return f(n);
+};
+
+let n = 12;
+console.log(numSquares(n));
