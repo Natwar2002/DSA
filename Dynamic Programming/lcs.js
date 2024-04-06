2e4fa90c96bbcbe8ce4914ad8a8eafe0c4a729a7
Natwar Patidar <natvarp6062@gmail.com>
2024-04-06 22:17:10 +0530

    add code

--- Dynamic Programming/lcs.js
+++ Dynamic Programming/lcs.js
@@ -1,7 +1,13 @@
 let dp;
 
 var f = function (s1, s2, i, j) {
-    
+    if(i == -1 || j == -1) return 0;
+    if(dp[i][j] != -1) return dp[i][j];
+    if(s1[i] == s2[j]) {
+        return dp[i][j] = 1 + f(s1, s2, i-1, j-1);
+    } else {
+        return dp[i][j] = Math.max(f(s1, s2, i-1, j), f(s1, s2, i, j-1));
+    }
 }
 
 var longestCommonSubsequebce = function (text1, text2) {
@@ -11,3 +17,4 @@ var longestCommonSubsequebce = function (text1, text2) {
     }
     return f(text1, text2, text1.length - 1, text2.length - 1)
 }
+
