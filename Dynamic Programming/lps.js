0864e2a6f66d409758640c3ead87649572397d41
Natwar Patidar <natvarp6062@gmail.com>
2024-04-07 00:41:10 +0530

    add code

--- Dynamic Programming/lps.js
+++ Dynamic Programming/lps.js
@@ -1,7 +1,14 @@
 let dp = new Array(1001);
 
 var f = function (s, i, j) {
-
+    if(i > j) return 0;
+    if(i == j) return 1;
+    if(dp[i][j] != -1) return dp[i][j];
+    if(s[i] == s[j]) {
+        return dp[i][j] = 2 + f(s, i+1, j-1);
+    } else {
+        return dp[i][j] = Math.max(f(s, i+1, j), f(s, i, j-1));
+    }
 }
 var longestPalindromicSubsequebce = function (s) {
     for(let i = 0; i < 1001; i++) {
@@ -9,3 +16,6 @@ var longestPalindromicSubsequebce = function (s) {
     }
     return f(s, 0, s.length);
 }
+
+let s = "abbabbabb";
+console.log(longestPalindromicSubsequebce(s));
