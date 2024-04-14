9a4ab8f4fc8f48edf739aed56f9b2a49b2eacf70
Natwar Patidar <natvarp6062@gmail.com>
2024-04-15 00:57:30 +0530

    add code

--- Stacks/stackUsingLL.js
+++ Stacks/stackUsingLL.js
@@ -9,7 +9,7 @@ var MyLinkedList = function (){
     this.head = null;
 }
 
-MyLinkedList.prototype.addAtHead (val) {
+MyLinkedList.prototype.addAtHead = function (val) {
     if(this.head == null) {
         this.head = new node(val);
         return;
@@ -19,7 +19,7 @@ MyLinkedList.prototype.addAtHead (val) {
     this.head = n;
 }
 
-MyLinkedList.prototype.deleteAtHead() {
+MyLinkedList.prototype.deleteAtHead = function () {
     if(this.head == null) return;
     let newHead = this.head.next;
     let nodeToBeDel = this.head;
@@ -27,6 +27,34 @@ MyLinkedList.prototype.deleteAtHead() {
     nodeToBeDel.next = null;
 }
 
+class Stack {
+    #ll;
+
+    constructor() {
+        this.#ll = new MyLinkedList();
+    }
+
+    push (element) {
+        this.#ll.addAtHead(element);
+    }
+
+    pop () {
+        this.#ll.deleteAtHead();
+    }
+
+    top () {
+        return this.#ll.head.val;
+    }
+
+    isEmpty () {
+        return this.#ll.head == null;
+    }
+}
 
 let s = new Stack();
+s.push(10);
+s.push(20);
+console.log(s.top());
+s.pop();
+console.log(s.top());
 console.log(s.isEmpty());
