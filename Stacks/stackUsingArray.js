fafa74d649e04a9cb99cee9a30dd3cd8cb75aaf6
Natwar Patidar <natvarp6062@gmail.com>
2024-04-15 00:10:24 +0530

    add code

--- Stacks/stackUsingArray.js
+++ Stacks/stackUsingArray.js
@@ -1,61 +1,27 @@
-var Node = function(data) {
-    this.data = data;
-    this.next = null; // until or unless we attach this new node to a list, the next property should be null
-}
-
-
-var MyLinkedList = function() {
-    this.head = null; // because initially everything is empty
-};
-
-/** 
- * @param {number} val
- * @return {void}
- */
-MyLinkedList.prototype.addAtHead = function(val) {
-    if(this.head == null) {
-        // means the ll is empty, so this node should become the head
-        this.head = new Node(val);
-    } else {
-        // when ll is not. empty
-        let n = new Node(val);
-        n.next = this.head;
-        this.head = n;
-    }
-};
-
-
-
-MyLinkedList.prototype.deleteAtHead = function() {
-    if(this.head == null) return;
-    let nextHead = this.head.next;
-    let nodeToBeDelete = this.head; // curr head
-    this.head = nextHead;
-    nodeToBeDelete.next = null; // prev head disconnected
-}
-
 class Stack {
-    #ll; // private linked list
-    constructor() {
-        this.#ll = new MyLinkedList();
-    }
+    // Private data member
+    #arr;
 
+    constructor() {
+        this.#arr = [];
+    }
 
     push(element) {
-        this.#ll.addAtHead(element);
+        this.#arr.push(element);
     }
+
     pop() {
-        this.#ll.deleteAtHead();
+        return this.#arr.pop();
     }
-    top() {
-        return this.#ll.head.data;
+
+    top(){
+        return this.#arr[this.#arr.length - 1];
     }
+
     isEmpty(){
-        return this.#ll.head == null;
+        return this.#arr.length == 0; 
     }
 }
-
-
 
 let s = new Stack();
 s.push(10);
@@ -64,7 +30,5 @@ s.push(30);
 console.log(s.top()); 
 s.pop(); // removes 30
 console.log(s.top());
-s.pop();
 console.log(s.top());
-
 console.log(s.isEmpty());
