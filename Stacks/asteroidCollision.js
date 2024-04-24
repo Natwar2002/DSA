040e775c769bc0a16a77f8ba47700d8df4f0a9ac
Natwar Patidar <natvarp6062@gmail.com>
2024-04-24 23:50:18 +0530

    add codes

---
+++ Stacks/asteroidCollision.js
@@ -0,0 +1,12 @@
+var asteroidCollision = function (a) {
+    let st = new Stack();
+    let i = 0;
+    while(i < a.length) {
+        if(st.isEmpty()) {
+            st.push(arr[i]);
+            i++;
+        } else {
+            if () {}
+        }
+    }
+}
