8bc2b47e1c6e70795360c4c59705875b8dba0bd1
Natwar Patidar <natvarp6062@gmail.com>
2024-04-25 23:35:39 +0530

    add codes

---
+++ Stacks/validParentheses.js
@@ -0,0 +1,44 @@
+class Stack {
+    #arr;
+    constructor() {
+        this.#arr = [];
+    }
+
+    push(element) {
+        this.#arr.push(element);
+    }
+
+    pop() {
+        return this.#arr.pop();
+    }
+
+    top () {
+        return this.#arr[this.#arr.length - 1];
+    }
+
+    isEmpty() {
+        return this.#arr.length == 0;
+    }
+}
+
+var validParentheses = function (s) {
+    let st = new Stack();
+    let i = 0;
+    while(i < s.length) {
+        let c = s[i];
+        if(c === '(' || c === '[' || c === '{') {
+            st.push(c);
+        } else {
+            if(c == ')' && st.top() == '(' || c == ']' && st.top() == '[' || c == '}' && st.top() == '{') {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        i++;
+    }
+    return st.isEmpty();
+} 
+
+let s = '(){}{}[][[]]';
+console.log(validParentheses(s));
