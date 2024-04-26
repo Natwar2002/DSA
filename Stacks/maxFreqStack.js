412483329ddc2fcb86886e53b7432b1507444ff1
Natwar Patidar <natvarp6062@gmail.com>
2024-04-27 00:14:44 +0530

    add codes

---
+++ Stacks/maxFreqStack.js
@@ -0,0 +1,42 @@
+class FreqStack {
+    freqMap = {};
+    max = 0;
+    elementMap = {};
+
+    push (val) {
+        if (!this.freqMap[val]) {
+            this.freqMap[val] = 1;
+        } else {
+            this.freqMap[val] += 1;
+        }
+
+        this.max = Math.max(this.max, this.freqMap[val]);
+        let frequency = this.freqMap[val];
+        if(!this.elementMap[frequency]) {
+            this.elementMap[frequency] = [val];
+        } else {
+            this.elementMap[frequency].push(val);
+        }
+    }
+
+    pop () {
+        let arr = this.elementMap[this.max];
+        let res = arr[arr.length - 1];
+        this.elementMap[this.max].pop();
+        this.freqMap[res]--;
+        if(this.elementMap[this.max].length == 0) {
+            delete this.elementMap[this.max];
+            this.max -= 1;
+        }
+        return res;
+    }
+}
+
+let st = new FreqStack();
+st.push(5);
+st.push(7);
+st.push(5);
+st.push(7);
+st.push(4);
+st.push(5);
+
