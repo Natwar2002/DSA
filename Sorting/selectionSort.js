6f68d80c5ce96f437238854c050b3bb4ba6e2022
Natwar Patidar <natvarp6062@gmail.com>
2024-04-30 19:31:44 +0530

    add codes

--- Sorting/selectionSort.js
+++ Sorting/selectionSort.js
@@ -1,23 +1,37 @@
 function selectionSort (arr) {
     let n = arr.length;
-    for (let i = 0; i < n - 1; i++) {
-        let minIndex = findMinElementIndex(arr, i);
-        if (minIndex !== i) {
-            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    // for (let i = 0; i < n - 1; i++) {
+    //     let minIndex = findMinElementIndex(arr, i);
+    //     if (minIndex !== i) {
+    //         [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    //     }
+    // }
+
+    for(let i = 0; i < n; i++) {
+        let min = i;
+        for(let j = i + 1; j < n; j++) {
+            if(arr[min] > arr[j]) {
+                min = j;
+            }
         }
-    }
+        if(min != i) {
+            let temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    } 
 }
 
-function findMinElementIndex (arr, i) {
-    let minElementIndex = i;
-    for (let j = i + 1; j < arr.length; j++) {
-        if(arr[j] < arr[minElementIndex]) {
-            minElementIndex = j;
-        }
-    }
-    return minElementIndex;
-}
+// function findMinElementIndex (arr, i) {
+//     let minElementIndex = i;
+//     for (let j = i + 1; j < arr.length; j++) {
+//         if(arr[j] < arr[minElementIndex]) {
+//             minElementIndex = j;
+//         }
+//     }
+//     return minElementIndex;
+// }
 
-let arr = [2,42,43,3,2,2,424,4];
+let arr = [5,6,7,8,1];
 selectionSort(arr);
 console.log(arr);
