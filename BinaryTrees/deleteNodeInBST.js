687900b760dbbb6ae24f73413b3347336051c59f
Natwar Patidar <natvarp6062@gmail.com>
2024-05-19 00:45:29 +0530

    add codes

--- BinaryTrees/deleteNodeInBST.js
+++ BinaryTrees/deleteNodeInBST.js
@@ -1,5 +1,33 @@
 var f = function(root, key) {
-    
+    if(!root) return null;
+    if(key < root.val) {
+        root.left = f(root.left, key);
+    } else if(key > root.val) {
+        root.right = f(root.right, key);
+    } else {
+        if(!root.left && !root.right) {
+            return null;
+        } else if (!root.left) {
+            return f(root.right);
+        } else if (!root.right) {
+            return f(root.left)
+        } else {
+            if(!root.left && !root.right) {
+                return null;
+            } else if (!root.left) {
+                return root.right;
+            } else if (!root.right) {
+                return root.left;
+            } else {
+                let temp = root.right;
+                while(temp.left != null) {
+                    temp = temp.left;
+                }
+                root.val = temp.val;
+                root.right = f(root.right, temp.val);
+            }
+        }
+    }
 }
 
 var deleteNode = function(root, key) {
