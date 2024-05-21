f42f8b165523a4335ccdd87eea853ee1c34452c6
Natwar Patidar <natvarp6062@gmail.com>
2024-05-21 15:27:56 +0530

    add codes

---
+++ BinaryTrees/invertBT.js
@@ -0,0 +1,14 @@
+function f (root) {
+    if(!root) return;
+    f(root.left);
+    f(root.right);
+
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+}
+
+var invertBinaryTree = function (root) {
+    f(root);
+    return root;
+};
