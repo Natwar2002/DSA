9a940dcaa2aed3e37b092993122f96df948baf54
Natwar Patidar <natvarp6062@gmail.com>
2024-05-21 21:20:02 +0530

    add codes

--- BinaryTrees/diameterOfBT.js
+++ BinaryTrees/diameterOfBT.js
@@ -1,15 +1,16 @@
-let ans = -1;
+let diameter;
 function f (root) {
     if(!root) return 0;
     let lst = f(root.left);
     let rst = f(root.right);
 
-    ans = Math.max(ans, lst + rst + 1);
+    diameter = Math.max(diameter, lst + rst);
 
     return 1 + Math.max(lst, rst);
 }
 
-var diameterOfBT = function (root) {
+var diameterOfBinaryTree = function (root) {
+    diameter = -1;
     f(root)
-    return ans - 1; 
+    return diameter; 
 };
