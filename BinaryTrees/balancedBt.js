77eb00759a152073361a9ea2633d7cfb055a4c31
Natwar Patidar <natvarp6062@gmail.com>
2024-05-23 11:13:00 +0530

    add codes

--- BinaryTrees/balancedBt.js
+++ BinaryTrees/balancedBt.js
@@ -1,7 +1,11 @@
 let ans;
 function f(root) {
     if(root == null) return 0;
-    
+    let lst = f(root.left);
+    let rst = f(root.right);
+
+    ans = ans && (Math.abs(lst - rst) <= 1);
+    return 1 + Math.max(lst, rst);
 }
 
 var isBalanced = function (root) {
