fd96fa6cb9bb61398d18d4921805714dc3411b19
Natwar Patidar <natvarp6062@gmail.com>
2024-05-24 12:10:50 +0530

    add codes

--- BinaryTrees/houseRobber3.js
+++ BinaryTrees/houseRobber3.js
@@ -5,7 +5,19 @@ function f (root, isRobbed) {
 
     if(isRobbed && mpTrue.get(root)) return mpTrue.get(root);
     if(!isRobbed && mpFalse.get(root)) return mpFalse.get(root);
-    
+
+    if(!isRobbed) {
+        const leave = f(root.left, false) + f(root.right, false);
+        const take = root.val + f(root.left, true) + f(root.right, true);
+        const ans = Math.max(leave, take);
+        mpFalse.set(root, ans);
+        return ans;
+    } else {
+        const leave = f(root.left, false) + f(root.right, false);
+        const ans = leave;
+        mpTrue.set(root, ans);
+        return ans;
+    }
 }
 
 var rob = function (root) {
