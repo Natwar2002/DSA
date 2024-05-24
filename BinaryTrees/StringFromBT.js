0e3ae31dbac4e051ee656ff70a3b711b17f0c3ec
Natwar Patidar <natvarp6062@gmail.com>
2024-05-24 15:21:12 +0530

    add codes

--- BinaryTrees/StringFromBT.js
+++ BinaryTrees/StringFromBT.js
@@ -1,20 +1,24 @@
-var f = function (root) {
+var f = function(root) {
     if(root == null) return "";
-    
+
     let left = f(root.left);
     let right = f(root.right);
 
-    if(left == "" && right == "") {
+    if(left == "" && right == ""){
         return `${root.val}`;
+        // return "" + root.val;
     } else if(left == "") {
         return `${root.val}()(${right})`;
-    } else if(right = "") {
+        // return "" + root.val + "()(" + right + ")";
+    } else if(right == "") {
         return `${root.val}(${left})`;
+        // return "" + root.val + "(" + left + ")";
     } else {
         return `${root.val}(${left})(${right})`;
+        // return "" + root.val + "(" + left + ")" + "(" + right + ")";
     }
 };
 
-var tree2str = function (root) {
+var tree2str = function(root) {
     return f(root);
-}
+};
