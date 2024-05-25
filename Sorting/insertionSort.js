304bea402dcdd8f64ee981ca072aac7f3312d06d
Natwar Patidar <natvarp6062@gmail.com>
2024-05-25 16:44:10 +0530

    add codes

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -2,6 +2,7 @@ function insertionSort (arr) {
     let n = arr.length;
     for (let i = 1; i < n; i++) {
         let element = arr[i];
+        let j;
         for(j = i-1; j >= 0; j--) {
             if (arr[j] > element) {
                 arr[j+1] = arr[j];
