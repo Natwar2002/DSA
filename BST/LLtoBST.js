847bb6051515cd4aef5ce8bd9f8fcab2249bf740
Natwar Patidar <natvarp6062@gmail.com>
2024-05-28 10:48:58 +0530

    add codes

---
+++ BST/LLtoBST.js
@@ -0,0 +1,22 @@
+function calcMid(head, tail) {
+    let slow = head, fast = head;
+    while(fast != tail && fast.next != tail) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+}
+
+function makeBST(head, tail) {
+    if(head == null) return null;
+    if(head == tail) return null;
+    let mid = calcMid(head, tail);
+    let root = new TreeNode(mid.val);
+    root.left = makeBST(head, mid);
+    root.right = makeBST(mid.next, tail);
+    return root;
+}
+
+var sortedListToBST = function (head) {
+    return makeBST(head, null);
+}
