658561c8c645afeaf27099f872eead1f14328e31
Natwar Patidar <natvarp6062@gmail.com>
2024-06-12 00:18:54 +0530

    Add codes

--- Trie/demo.js
+++ Trie/demo.js
@@ -12,14 +12,37 @@ class Trie {
     }
 
     insert(word) {
-
+        let cur = this.root;
+        for(let i = 0; i < word.length; i++){
+            const c = word[i];
+            if(cur.children[c]) {
+                cur = cur.children[c];
+            } else {
+                cur.children[c] = new node(c);
+                temp = cur.children[c];
+            }
+        }
+        cur.isTerminal = true;
     }
 
     search(word) {
-
+        let cur = this.root;
+        for(let i = 0; i < word.length; i++) {
+            const c = word[i];
+            if(cur.children[c]) {
+                cur = cur.children[c];
+            } else {
+                return false;
+            }
+        }
+        return cur.isTerminal;
     }
 
     startsWith(prefix) {
-        
+        let cur = this.root;
+        for(let i = 0; i < prefix.length; i++) {
+            const c = prefix[i];
+            if(cur)
+        }
     }
 }
