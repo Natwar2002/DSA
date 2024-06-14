7cf39550303fa49797a8e3523e7d74fe1bdc2362
Natwar Patidar <natvarp6062@gmail.com>
2024-06-14 23:43:57 +0530

    deletion

--- Heap/maxHeap.js
+++ Heap/maxHeap.js
@@ -3,7 +3,7 @@ class maxHeap {
         this.arr = [];
     }
 
-    upheapify(i) {
+    upHeapify(i) {
         while(i > 0) {
             let pi = Math.floor(i - 1 / 2);
             if(this.arr[pi] < this.arr[i]) {
@@ -15,8 +15,42 @@ class maxHeap {
         }
     }
 
-    insert(data) {
+    insert (data) {
         this.arr.push(data);
-        this.upheapify(this.arr.length - 1);
+        this.upHeapify(this.arr.length - 1);
+    }
+
+    downHeapify (i) {
+        while(i < this.arr.length - 1) {
+            let lc = i*2 + 1;
+            let rc = i*2 + 2;
+            let max = i;
+            if(lc < this.arr.length - 1 && this.arr[lc] > this.arr[max]) max = lc;
+            if(rc < this.arr.length - 1 && this.arr[rc] > this.arr[max]) max = rc;
+            if(i != max) {
+                [this.arr[]]
+            }
+        }
+    }
+
+    remove () {
+        [this.arr[0], this.arr[this.arr.length - 1]] = [this.arr[this.arr.length - 1], this.arr[0]];
+        this.arr.pop();
+        this.downHeapify(0);
+    }
+
+    display() {
+        console.log(this.arr);
     }
 }
+
+let hp = new maxHeap();
+hp.insert(10);
+hp.insert(40);
+hp.insert(3);
+hp.insert(20);
+hp.insert(100);
+hp.insert(50);
+hp.insert(6);
+hp.insert(4);
+hp.display();
