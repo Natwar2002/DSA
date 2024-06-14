ce815a4f089d776c7a2dc895dcc2c16489769bbd
Natwar Patidar <natvarp6062@gmail.com>
2024-06-14 23:28:40 +0530

    Upheapification

--- Heap/maxHeap.js
+++ Heap/maxHeap.js
@@ -4,7 +4,15 @@ class maxHeap {
     }
 
     upheapify(i) {
-        
+        while(i > 0) {
+            let pi = Math.floor(i - 1 / 2);
+            if(this.arr[pi] < this.arr[i]) {
+                [this.arr[pi], this.arr[i]] = [this.arr[i], this.arr[pi]]
+            } else {
+                break;
+            }
+            i = pi;
+        }
     }
 
     insert(data) {
