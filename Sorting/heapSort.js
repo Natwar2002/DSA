759dab76497b25f21379535387f74ee02f4b3e8b
Natwar Patidar <natvarp6062@gmail.com>
2024-06-18 23:55:11 +0530

    Add codes

--- Sorting/heapSort.js
+++ Sorting/heapSort.js
@@ -26,8 +26,8 @@ class Heap {
             let lc = i*2 + 1;
             let rc = i*2 + 2;
             let max = i;
-            if(lc < this.arr.length - 1 && this.cmp(this.arr[lc], this.arr[max])) max = lc;
-            if(rc < this.arr.length - 1 && this.cmp(this.arr[rc], this.arr[max])) max = rc;
+            if(lc < this.arr.length - 1 && this.cmp(this.arr[max], this.arr[lc])) max = lc;
+            if(rc < this.arr.length - 1 && this.cmp(this.arr[max], this.arr[rc])) max = rc;
             if(i != max) {
                 [this.arr[max], this.arr[i]] = [this.arr[i], this.arr[max]];
             } else break;
@@ -48,5 +48,17 @@ class Heap {
 }
 
 function heapSort (arr) {
-    
+    let n = arr.length;
+    const hp = new Heap((a, b) => a > b);
+    for(let i = 0; i < n; i++) {
+        hp.insert(arr[i]);
+    }
+    for(let i = 0; i < n; i++) {
+        arr[i] = hp.root();
+        hp.remove();
+    }    
 }
+
+let arr = [5, 3, 8, 4, 2];
+heapSort(arr);
+console.log(arr);
