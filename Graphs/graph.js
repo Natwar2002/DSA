709715df50550a9493925a03b0ee6367d4914bd7
Natwar Patidar <natvarp6062@gmail.com>
2024-06-20 00:30:37 +0530

    Add codes

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -1,9 +1,9 @@
 class Graph {
     constructor(noOfVertices) {
         this.v = noOfVertices;
-        this.adjList = new Array(this.v)
+        this.adjList = new Array(this.v);
         for(let i = 0; i < this.v; i++) {
-            this.addEdge[i] = [];
+            this.adjList[i] = [];
         }
     }
 
@@ -20,3 +20,13 @@ class Graph {
 }
 
 let g = new Graph(7);
+g.addEdge(0, 1);
+g.addEdge(0, 6);
+g.addEdge(6, 5);
+g.addEdge(1, 5);
+g.addEdge(1, 2);
+g.addEdge(2, 4);
+g.addEdge(2, 3);
+g.addEdge(4, 3);
+
+g.display();
