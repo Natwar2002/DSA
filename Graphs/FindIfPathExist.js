4a8e362b17df5cec7aa30afec7c5bc6664f3cd06
Natwar Patidar <natvarp6062@gmail.com>
2024-06-21 00:17:05 +0530

    Add codes

---
+++ Graphs/FindIfPathExist.js
@@ -0,0 +1,16 @@
+
+
+var validPath = function (n, edges, source, destination) {
+    let g = new Array(n);
+    for(let i = 0; i < n; i++) {
+        g[i] = [];
+    }
+    for(let i = 0; i < edges.length; i++) {
+        let u = edges[i][0];  // Begining of edge
+        let v = edges[i][1];  // Termination of edge
+        g[u].push(v);
+        g[v].push(u);
+    }
+
+    return dfs(source, destination, g, new Set());
+};
