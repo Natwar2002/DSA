39e6b40d43786f18bd70804b288a51cdc554c394
Natwar Patidar <natvarp6062@gmail.com>
2024-06-21 10:36:59 +0530

    Add codes

---
+++ Dynamic Programming/triangle.js
@@ -0,0 +1,17 @@
+let matrix;
+let dp;
+
+function f (row, col) {
+    if(row == matrix.length - 1) return matrix[row][col];
+    if(dp[row][col] != -1) return dp[row][col];
+    return dp[row][col] = matrix[row][col] + Math.min(f(row + 1, col), f(row + 1, col + 1));
+}
+
+var minimumTotal = function (triangle) {
+    matrix = triangle;
+    dp = new Array(205);
+    for(let i = 0; i < 205; i++) {
+        dp[i] = new Array(205).fill(-1);
+    }
+    return f(0, 0);
+}
