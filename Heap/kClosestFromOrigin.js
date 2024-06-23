c300a8c127ec5c2d9ae115f5ffb824802ff919a8
Natwar Patidar <natvarp6062@gmail.com>
2024-06-23 23:53:47 +0530

    Add codes

---
+++ Heap/kClosestFromOrigin.js
@@ -0,0 +1,66 @@
+class Heap {
+    constructor (cmp) {
+        this.arr = [];
+        this.cmp = cmp;
+    }
+
+    upHeapify(i) {
+        while(i > 0) {
+            let pi = Math.floor(i - 1 / 2);
+            if(this.cmp(this.arr[pi], this.arr[i])) {
+                [this.arr[pi], this.arr[i]] = [this.arr[i], this.arr[pi]]
+            } else {
+                break;
+            }
+            i = pi;
+        }
+    }
+
+    insert (data) {
+        this.arr.push(data);
+        this.upHeapify(this.arr.length - 1);
+    }
+
+    downHeapify (i) {
+        while(i < this.arr.length - 1) {
+            let lc = i*2 + 1;
+            let rc = i*2 + 2;
+            let max = i;
+            if(lc < this.arr.length - 1 && this.cmp(this.arr[lc], this.arr[max])) max = lc;
+            if(rc < this.arr.length - 1 && this.cmp(this.arr[rc], this.arr[max])) max = rc;
+            if(i != max) {
+                [this.arr[max], this.arr[i]] = [this.arr[i], this.arr[max]];
+            } else break;
+
+            i = max;
+        }
+    }
+
+    remove () {
+        [this.arr[0], this.arr[this.arr.length - 1]] = [this.arr[this.arr.length - 1], this.arr[0]];
+        this.arr.pop();
+        this.downHeapify(0);
+    }
+
+    isEmpty() {
+        return this.arr.length == 0;
+    }
+    
+    display() {
+        console.log(this.arr);
+    }
+}
+
+class Point {
+    constructor(xi, yi) {
+        this.x = xi;
+        this.y = yi;
+        this.dist = Math.sqrt((xi * xi) + (yi * yi));
+    }
+}
+
+var kClosest = function(points, k) {
+    let hp = new Heap((pointA, pointB) => {
+        return pointA.dist >
+    })
+};
