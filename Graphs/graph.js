52960d176a5244bc0ae58f06669cfca643e39866
Natwar Patidar <natvarp6062@gmail.com>
2024-06-30 23:33:40 +0530

    Add codes

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -2,14 +2,14 @@ class Graph {
     constructor(noOfVertices) {
         this.v = noOfVertices;
         this.adjList = new Array(this.v);
-        for(let i = 0; i < this.v; i++) {
+        for (let i = 0; i < this.v; i++) {
             this.adjList[i] = [];
         }
     }
 
     addEdge(v1, v2, biDir = true) {
         this.adjList[v1].push(v2);
-        if(biDir){
+        if (biDir) {
             this.adjList[v2].push(v1);
         }
     }
