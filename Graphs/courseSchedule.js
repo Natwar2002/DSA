6158a3b164906bd69c0ee15210850dbbda5d2794
Natwar Patidar <natvarp6062@gmail.com>
2024-07-03 20:33:34 +0530

    Add codes

--- Graphs/courseSchedule.js
+++ Graphs/courseSchedule.js
@@ -1,5 +1,27 @@
-function topoSort() {
-
+function topoSort(g, n, indegree) {
+    let q = [];
+    let vis = new Set();
+    for(let i = 0; i < n; i++) {
+        if(indegree[i] == 0) {
+            q.push(i);
+            vis.add(i);
+        }
+    }
+    let c = 0;
+    while(q.length > 0) {
+        let cur = q.shift();
+        c++;
+        for(let neighbour of g[cur]) {
+            if(!vis.has(neighbour)) {
+                indegree[neighbour]--;
+                if(indegree[neighbour] == 0) {
+                    q.push(neighbour);
+                    vis.add(neighbour);
+                }
+            }
+        }
+    }
+    return c == n;
 }
 
 var canFinish = function(numCourses, prerequisites) {
@@ -12,6 +34,7 @@ var canFinish = function(numCourses, prerequisites) {
         let a = cur[0];
         let b = cur[1];
         g[b].push(a);
+        indegree[a]++;
     }
     return topoSort(g, numCourses, indegree);
 }
