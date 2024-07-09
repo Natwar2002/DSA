880100e3897c3d3106e4c6f73ad848c59effc2ad
Natwar Patidar <natvarp6062@gmail.com>
2024-07-09 23:48:04 +0530

    Add codes

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -1,20 +1,20 @@
-function insertionSort (arr) {
+function insertionSort(arr) {
     let n = arr.length;
     for (let i = 1; i < n; i++) {
         let element = arr[i];
         let j;
-        for(j = i-1; j >= 0; j--) {
+        for (j = i - 1; j >= 0; j--) {
             if (arr[j] > element) {
-                arr[j+1] = arr[j];
+                arr[j + 1] = arr[j];
             } else {
                 // arr[j] <= element;
                 break;
             }
         }
-        arr[j+1] = element;
+        arr[j + 1] = element;
     }
 }
 
-let arr = [5,6,7,8,1];
+let arr = [5, 6, 7, 8, 1];
 insertionSort(arr);
 console.log(arr);
