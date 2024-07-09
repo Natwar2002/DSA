7650e2d75a6523b855a1af544cac7caaa5368007
Natwar Patidar <natvarp6062@gmail.com>
2024-07-09 23:47:46 +0530

    Add codes

--- Sorting/heapSort.js
+++ Sorting/heapSort.js
@@ -1,13 +1,13 @@
 class Heap {
-    constructor (cmp) {
+    constructor(cmp) {
         this.arr = [];
         this.cmp = cmp;
     }
 
     upHeapify(i) {
-        while(i > 0) {
+        while (i > 0) {
             let pi = Math.floor((i - 1) / 2);
-            if(this.cmp(this.arr[pi], this.arr[i])) {
+            if (this.cmp(this.arr[pi], this.arr[i])) {
                 [this.arr[pi], this.arr[i]] = [this.arr[i], this.arr[pi]];
                 i = pi;
             } else {
@@ -16,27 +16,27 @@ class Heap {
         }
     }
 
-    insert (data) {
+    insert(data) {
         this.arr.push(data);
         this.upHeapify(this.arr.length - 1);
     }
 
-    downHeapify (i) {
-        while(i < this.arr.length) {
-            let lc = i*2 + 1;
-            let rc = i*2 + 2;
+    downHeapify(i) {
+        while (i < this.arr.length) {
+            let lc = i * 2 + 1;
+            let rc = i * 2 + 2;
             let max = i;
-            if(lc < this.arr.length && this.cmp(this.arr[max], this.arr[lc])) max = lc;
-            if(rc < this.arr.length && this.cmp(this.arr[max], this.arr[rc])) max = rc;
-            if(i != max) {
+            if (lc < this.arr.length && this.cmp(this.arr[max], this.arr[lc])) max = lc;
+            if (rc < this.arr.length && this.cmp(this.arr[max], this.arr[rc])) max = rc;
+            if (i != max) {
                 [this.arr[max], this.arr[i]] = [this.arr[i], this.arr[max]];
                 i = max;
             } else break;
         }
     }
 
-    remove () {
-        if(this.arr.length == 0) return;
+    remove() {
+        if (this.arr.length == 0) return;
         [this.arr[0], this.arr[this.arr.length - 1]] = [this.arr[this.arr.length - 1], this.arr[0]];
         this.arr.pop();
         this.downHeapify(0);
@@ -47,16 +47,16 @@ class Heap {
     }
 }
 
-function heapSort (arr) {
+function heapSort(arr) {
     let n = arr.length;
     const hp = new Heap((a, b) => a > b);
-    for(let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         hp.insert(arr[i]);
     }
-    for(let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         arr[i] = hp.root();
         hp.remove();
-    }    
+    }
 }
 
 let arr = [5, 3, 8, 4, 2];
