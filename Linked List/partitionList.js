d08133c790bfc330cc03623671d0a267eca82d4f
Natwar Patidar <natvarp6062@gmail.com>
2024-07-10 21:05:15 +0530

    Add codes

--- Linked List/partitionList.js
+++ Linked List/partitionList.js
@@ -1,11 +1,11 @@
-var partition = function(head, x) {
+var partition = function (head, x) {
     let d1 = new ListNode(), d2 = new ListNode(), t1 = d1, t2 = d2, cur = head;
     while (cur != null) {
         let temp = cur;
         cur = cur.next;
         temp.next = null;
 
-        if(temp.val < x) {
+        if (temp.val < x) {
             t1.next = temp;
             t1 = t1.next;
         } else {
