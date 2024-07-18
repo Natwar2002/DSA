86f38055a156135081d8b98b2079e6f5bbd1b083
Natwar Patidar <natvarp6062@gmail.com>
2024-07-18 23:58:54 +0530

    add code

--- Strings/multiplyStrings.js
+++ Strings/multiplyStrings.js
@@ -1,15 +1,15 @@
 function multiplyStrings(num1, num2) {
     // If either number is "0", the product is "0"
     if (num1 === "0" || num2 === "0") return "0";
-    
+
     // Initialize the result array with zeros
     let m = num1.length, n = num2.length;
     let result = new Array(m + n).fill(0);
-    
+
     // Reverse the numbers to make it easier to calculate from right to left
     num1 = num1.split('').reverse().join('');
     num2 = num2.split('').reverse().join('');
-    
+
     // Multiply each digit of the first number with each digit of the second number
     // num1 = 111    num2 = 222
     for (let i = 0; i < m; i++) {
@@ -20,12 +20,12 @@ function multiplyStrings(num1, num2) {
             result[i + j + 1] += Math.floor(sum / 10);
         }
     }
-    
+
     // Remove leading zeros from the result array and convert to a string
     while (result.length > 1 && result[result.length - 1] === 0) {
         result.pop();
     }
-    
+
     return result.reverse().join('');
 }
 
