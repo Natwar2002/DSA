f6552d9c409f7ec9ae31829f5f9ee92fede5dea6
Natwar Patidar <natvarp6062@gmail.com>
2024-07-18 23:48:49 +0530

    add code

--- random.js
+++ random.js
@@ -1,4 +1,9 @@
 let start = 1;
 let end = 10;
-let randomNumber = Math.floor(Math.random() * (end - start) + start);
-console.log(randomNumber);
+// let randomNumber = Math.floor(Math.random() * (end - start) + start);
+// console.log(randomNumber);
+
+let n = Math.floor(Math.random() * 16);
+let f = n.toString(16);
+console.log(f);
+console.log(n);
