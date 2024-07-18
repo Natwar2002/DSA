f9b0b9a8e8dfa6ace84d2950f7ee1a15a2144398
Natwar Patidar <natvarp6062@gmail.com>
2024-07-18 23:59:08 +0530

    add code

--- Strings/noOfOccurances.js
+++ Strings/noOfOccurances.js
@@ -1,7 +1,7 @@
 var mapping = function (s) {
     let mp = {};
-    for(let i = 0; i < s.length; i++) {
-        if(mp[s[i]]) {
+    for (let i = 0; i < s.length; i++) {
+        if (mp[s[i]]) {
             mp[s[i]] += 1;
         } else {
             mp[s[i]] = 1;
